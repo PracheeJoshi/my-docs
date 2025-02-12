ca02f53ac35e0aa645c02d3792cca3ee79b78d05
PracheeJoshi <prachee.joshi@karkinos.in>
2025-02-12 16:36:28 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  module.exports = {
+  url: 'https://PracheeJoshi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-docs/',
+  projectName: 'my-docs',
+  organizationName: 'PracheeJoshi',
+};
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
